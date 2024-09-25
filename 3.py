29070a48a444ac5ff3769894669553ea8a88d026
rehanv519 <rehanvalora123@gmail.com>
2024-09-25 15:26:20 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,30 @@
+a = "Hello, World!"   #String As Arrays
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+print(a[7])
+print(a[8])
+print(a[9])
+print(a[10])
+print(a[11])
+print(a[12])
+#for x in "apple":    #looping as String
+#print(x)
+a = "Hello, World!"  #length of string
+print(len(a))
+txt = "The best thing"  #check string
+print("o" in txt)
+"""t = "The best things in life"    #check string using if statement
+if  "in" in t:
+print("Yes, 'in' is present.") """
+txt = "The best things"      #check string using not statement    
+print("r" not in txt)
+x = "The things"         #check string if the alphabet is not present then it prints
+if "c" not in x:
+  print("No, 'c' is NOT present.")
+              
+              
