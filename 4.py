eb5aacbf00ef1706478536a07acbfbfccd455afb
rehanv519 <rehanvalora123@gmail.com>
2024-09-26 15:21:11 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,43 @@
+b = "Hello,World!"   #Slicing Of String
+print(b[3:4])
+b = "HelloWorld"    #Slice From Start
+print(b[:6])
+b = "Hello, !  World"  #Slice To The End
+print(b[4:])
+b = "rehan,valora"     #Negative Indexing
+print(b[-3:-2])
+a = "HeLlOWoRlD"     #UpperCase
+print(a.upper())
+a = "HeLlOWoRlD"    #LowerCase
+print(a.lower())
+a = " Hello, World! "    #Whitespace Removed From The Beginning And Ending 
+print(a.strip())
+a = "Employee"        #Replacement Of String
+print(a.replace("Em", "De"))
+a = "Hello, World!"     #Splitting Of The String
+b = a.split("W")
+print(b)
+a = "Hello"   #Concatenation Of The String
+b = "World"
+c = a + b
+print(c)
+a = "Hello"   #2-> Way
+b = "World"
+print(a,b)
+age = 19     #F String
+txt = f"My name is Rehan, I am {age} years old."
+print(txt)
+price = 500       # Placeholders And Modifiers
+txt = f"This is {price} rupees"
+print(txt)
+price = 10      #Adding Decimals After...
+txt = f"This is {price:.2f} dollars"
+print(txt)
+txt = f"The answer of 20/59  is {20 / 59} "    #Make Calculations With It
+print(txt)
+
+
+
+
+
+
