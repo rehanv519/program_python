c0a8d409af1bce1df16885cad12cc01935503803
rehanv519 <rehanvalora123@gmail.com>
2024-09-28 11:18:33 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,75 @@
+#ARITHMETIC OPERATOR
+print(67+42) 
+print(67-42) 
+print(67*42) 
+print(67/42) 
+print(67%42) 
+print(67**42) 
+print(67//42) 
+#ASSIGNMENT OPERATOR
+x=59
+print(x)
+x+=3
+print(x)
+x-=3
+print(x)
+x*=3
+print(x)
+x/=3
+print(x)
+x%=3
+print(x)
+x//=3
+print(x)
+x**=3
+print(x)
+x=5
+x&=3
+print(x)
+x=5
+x|=3
+print(x)
+x=5
+x^=3
+print(x)
+x=5
+x<<=3
+print(x)
+x=5
+x>>=3
+print(x)
+print(x:=3)
+#COMPARISION OPERATOR
+x=3
+y=5
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+#LOGICAL OPERATOR
+print(x > 3 and x < 10)
+print(x > 3 or x < 10)
+print(not(x > 3 and x < 10))
+#IDENTITY OPERATOR
+x=5
+y=10
+print(x is y)
+print(x == y)
+print(x is not y)
+print(x != y)
+#MEMBERSHIP OPERATORS
+x = ["apple", "banana"]
+print("PINEAPPLE" in x)
+x = ["apple", "banana"]
+print("apple" not in x)
+#BITWISE OPERATOR
+print(6 & 3)
+print(6 | 3)
+print(6 ^ 3)
+print(~6)
+print(6<<3)
+print(6>>3)
+
+
