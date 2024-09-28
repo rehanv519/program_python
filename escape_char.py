266d7ba36e77e91e82ec0925d05a22124d1b2dec
rehanv519 <rehanvalora123@gmail.com>
2024-09-28 11:36:45 +0530

    Add files via upload

---
+++ escape_char.py
@@ -0,0 +1,18 @@
+txt= "Hello \"World!\""
+print(txt)
+txt = 'Hello\'s World!.' #SingleQuote
+print(txt) 
+txt = "Hello\\(World)." #BackSlash
+print(txt) 
+txt = "Hello\nWorld!" #newline
+print(txt) 
+txt = "Hello\rWorld!" #CarriageReturn
+print(txt) 
+txt = "Hello\tWorld!" #Tab
+print(txt) 
+txt = "Hello\bWorld!"  #Backslash
+print(txt) 
+txt = "\110\145\154\154\157 \167\157\162\154\144" 
+print(txt) 
+txt = "\x48\x65\x6c\x6c\x6f "   #HexVal
+print(txt) 
