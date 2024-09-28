2621a9b4a787ea000907cc8dc1d2041a29234479
rehanv519 <rehanvalora123@gmail.com>
2024-09-28 12:09:07 +0530

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,44 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+a = 200
+b = 33
+if b > a:    #Condition
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+print(bool("Hello"))    #Values And Variables
+print(bool(15))
+print(bool("abc"))    #Most Values Here Are True
+print(bool(123))
+print(bool(0))
+print(bool(["apple", "cherry", "banana"]))
+print(bool(False))   
+print(bool(None))
+print(bool(""))
+print(bool())
+print(bool([]))
+print(bool({}))
+print(bool(20))
+print(bool(20.5))
+print(bool("Hello World"))
+print(bool(1j))
+print(bool( ["apple", "banana", "cherry"]))
+print(bool(("apple", "banana", "cherry")))
+print(bool(range(6)))
+print(bool({"name" : "John", "age" : 36}))
+print(bool({"apple", "banana", "cherry"}))
+print(bool(frozenset({"apple", "banana", "cherry"})))
+print(bool( True))
+print(bool(b"Hello"))
+print(bool(bytearray(5)))
+print(bool(memoryview(bytes(5))))
+print(bool(None))
+
+
+
+
+
+
+
+  
