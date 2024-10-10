9aa6c035b23f81bc681189319978a9cfd6dd31aa
rehanv519 <rehanvalora123@gmail.com>
2024-10-10 15:06:14 +0530

    Add files via upload

---
+++ if..else.py
@@ -0,0 +1,24 @@
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+x=60
+y=80
+if(x>y):
+    print('X is greater than Y')
+if (x>40):
+    print('x is greater than 40')
+if(y<100):
+    print('y is less than 100')
+if(y>x):
+    print('y is greater than x') 
+if(y>50):
+    print('y is grater than 50')            
+a = 33 #Pass Statement
+b = 200
+if b > a:
+pass
+
+    
