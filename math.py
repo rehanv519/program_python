73b8f403198562ee95df1669dd2ed29cad901f58
rehanv519 <rehanvalora123@gmail.com>
2024-10-22 14:56:05 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,18 @@
+x = min(5, 10, 25)  #Min
+y = max(5, 10, 25)  #Max
+print(x)
+print(y)
+x = abs(-7.25)  #Absolute
+print(x)
+x = pow(4, 3)  #Power
+print(x)
+import math  #Square
+x = math.sqrt(64)
+import math
+x = math.ceil(1.4)  #Ceil
+y = math.floor(1.4)  #Floor
+print(x)
+print(y)
+import math  #Pi
+x = math.pi
+print(x)
