62442fa0899d0cbec70367abc794ce7c13dc79a9
rehanv519 <rehanvalora123@gmail.com>
2024-10-22 14:25:52 +0530

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,55 @@
+x = "Hello World!" #Function Polymorphism
+print(len(x))
+mytuple = ("apple", "banana", "cherry") #Tuple
+print(len(mytuple))
+thisdict = {    #Dictionary
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(len(thisdict))
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+  def move(self):
+    print("Drive!")
+class Boat:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Sail!")
+class Plane:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+  def move(self):
+    print("Fly!")
+car1 = Car("Ford", "Mustang")       #Create a Car class
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
+plane1 = Plane("Boeing", "747")     #Create a Plane class
+for x in (car1, boat1, plane1):
+  x.move()
+class Vehicle:  #Inheritance Class Polymorphism
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+  def move(self):
+    print("Move!")
+class Car(Vehicle):
+  pass
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
